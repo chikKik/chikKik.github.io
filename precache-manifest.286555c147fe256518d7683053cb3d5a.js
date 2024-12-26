95e3b9b804f697585c967a984e9f42f6433436cb
AChikina <49834573+chikKik@users.noreply.github.com>
2024-12-26 13:31:58 +0300

    Add files via upload

---
+++ precache-manifest.286555c147fe256518d7683053cb3d5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e36f5319b998c31e132c8d0bcffb4a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e0c130a8fcc2e06a51d",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "1e0c130a8fcc2e06a51d",
+    "url": "/static/js/main.8f727e7e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
